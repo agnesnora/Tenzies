951f6362e20195ae7e597af2909e76a52ac99977
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-05-24 13:07:50 +0200

    setting up css

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,25 +1,39 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Dice from "../src/Components/Dice";
+
+/**
+ * Challenge:
+ *
+ * - Create a Die component that takes a `value` prop
+ * - Render 10 instances of the Die component (manually)
+ *      - Provide a number between 1-6 for the value on each
+ *        for now
+ * - Style the <main> and <Die> components
+ *   to look like they do in the slide
+ *      - Hints: Create a container to hold the 10 instances
+ *        of the Die component, and use CSS Grid to lay them
+ *        out evenly in 2 rows of 5 columns
+ *      - Use flexbox on main to center the dice container
+ *        in the center of the page
+ */
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div className="dice--container">
+          <Dice value={3} />
+          <Dice value={6} />
+          <Dice value={2} />
+          <Dice value={3} />
+          <Dice value={3} />
+          <Dice value={1} />
+          <Dice value={3} />
+          <Dice value={5} />
+          <Dice value={3} />
+          <Dice value={4} />
+        </div>
+      </main>
     </>
   );
 }
